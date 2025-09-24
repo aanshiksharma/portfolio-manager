ad9f29c791bc377cbd4c406da34c97c8fa172848
Aanshik Sharma <aanshik16@gmail.com>
2025-09-25 03:28:36 +0530

    Connected MongoDB with the backend

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -8,6 +9,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+
+// MongoDB connection
+const connectToDatabse = async () => {
+  const MONGO_URI = process.env.MONGO_URI;
+
+  if (!MONGO_URI) {
+    console.error("MONGO_URI is not defined in the environment variables.");
+    return;
+  }
+
+  try {
+    const res = await mongoose.connect(MONGO_URI);
+    if (res) console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("Error connecting to MongoDB", err);
+  }
+};
+
+connectToDatabse();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
