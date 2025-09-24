1fb528b5ee274519b1e46d12d91d091dc6469baf
Aanshik Sharma <aanshik16@gmail.com>
2025-09-24 21:25:08 +0530

    Basic Frontend Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-neutral-950">
+        <h1 className="text-4xl font-bold text-neutral-400">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
