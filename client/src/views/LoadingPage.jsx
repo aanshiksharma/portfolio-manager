51cbb8fecb2b79c1f6223515f98a25876ed1f153
Aanshik Sharma <aanshik16@gmail.com>
2025-10-01 20:45:10 +0530

    Added basic loading page. Added Skills page.

---
+++ client/src/views/LoadingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoadingPage() {
+  return (
+    <>
+      <div className="text-text-primary flex items-center justify-center h-screen w-full">
+        Loading...
+      </div>
+    </>
+  );
+}
+
+export default LoadingPage;
