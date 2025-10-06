bc5dcbe0e3a56e13f31b37090407101857c7c0ca
Aanshik Sharma <aanshik16@gmail.com>
2025-10-06 23:27:30 +0530

    Added origin to cors middleware

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://portfolio-manager-cms.vercel.app/",
+  })
+);
 app.use(express.json());
 
 // MongoDB connection
