5488a1cac2e9e1749c9c1240d7be7e6fce9137ee
Aanshik Sharma <aanshik16@gmail.com>
2025-10-06 23:34:42 +0530

    Cors fix

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "https://portfolio-manager-cms.vercel.app/",
+    origin: "https://portfolio-manager-cms.vercel.app",
   })
 );
 app.use(express.json());
