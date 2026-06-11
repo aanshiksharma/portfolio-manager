f96124ee092cc6247942acfec170f0d4a3849736
Aanshik Sharma <aanshik16@gmail.com>
2026-06-11 18:22:58 +0530

    Project Page redesign complete

--- client/src/features/projects/pages/ProjectPage.jsx
+++ client/src/features/projects/pages/ProjectPage.jsx
@@ -1,72 +1,127 @@
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import useProject from "../hooks/useProject";
 
-import Button from "../../../shared/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import LoadingScreen from "../../../shared/components/ui/LoadingScreen";
+import useImageViewer from "@/shared/image-viewer/useImageViewer";
 
-import ProjectDetails from "../components/ProjectDetails";
-import ProjectGallery from "../components/ProjectGallery";
+import { Item, ItemContent, ItemHeader, ItemTitle } from "@/components/ui/item";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Badge } from "@/components/ui/badge";
+
+import { Github } from "react-bootstrap-icons";
+import { ExternalLink } from "lucide-react";
 
 function ProjectPage() {
-  const [activeTab, setActiveTab] = useState("general");
-
-  const navigate = useNavigate();
   const location = useLocation();
   const pathname = location.pathname.split("/");
   const projectId = pathname[pathname.length - 1];
 
   const { project, loading } = useProject({ projectId });
+  const canHover = window.matchMedia("(hover: hover)").matches;
+  const { open } = useImageViewer();
 
   if (loading || !project) return <LoadingScreen />;
 
   return (
     <>
-      <div className="container">
-        <div className="flex flex-col gap-4 px-4 py-8 w-full">
-          <section className="flex items-center justify-between">
-            <h1 className="text-[2rem] text-text-primary font-medium">
-              {project.title}
-            </h1>
+      <section className="space-y-4">
+        <div
+          className="relative -z-0 rounded-2xl h-[50vh] overflow-hidden"
+          onClick={() => {
+            open(project.coverImage.url);
+          }}
+        >
+          <img
+            src={project.coverImage.url}
+            alt={`${project.title} cover image`}
+            className="h-full w-full object-cover"
+          />
 
-            <div className="flex gap-2 items-center">
-              <Button
-                label={"Edit Project"}
-                variant={"primary"}
-                onClick={() => {
-                  navigate(`/projects/edit/${project._id}`);
-                }}
-              />
-            </div>
-          </section>
-
-          <section className="flex items-center gap-3">
-            {["general", "gallery"].map((tab) => {
-              return (
-                <Button
-                  key={tab}
-                  variant={tab === activeTab ? "primary" : "secondary"}
-                  icon={{
-                    icon: tab === "general" ? "database" : "images",
-                    size: 16,
-                  }}
-                  label={tab.charAt(0).toUpperCase() + tab.slice(1)}
-                  onClick={() => {
-                    setActiveTab(tab);
-                  }}
-                />
-              );
-            })}
-          </section>
-
-          {/* General Details Section */}
-          {activeTab === "general" && <ProjectDetails project={project} />}
-
-          {/* Gallery Section */}
-          {activeTab === "gallery" && <ProjectGallery project={project} />}
+          <div className="absolute inset-0 bg-linear-to-b from-background/10 from-50% to-95% to-background inset-shadow-2xs inset-shadow-accent-foreground/20 rounded-2xl" />
         </div>
-      </div>
+
+        <div className="flex items-center justify-between">
+          <h1>{project.title}</h1>
+
+          <div className="flex items-center gap-2 flex-wrap">
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  variant="secondary"
+                  size={canHover ? "default" : "icon"}
+                  asChild
+                >
+                  <a href={project.projectLink}>
+                    <ExternalLink />
+                    <span className="[@media(hover:hover)]:inline hidden">
+                      Live
+                    </span>
+                  </a>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>{project.projectLink}</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  variant="outline"
+                  size={canHover ? "default" : "icon"}
+                  asChild
+                >
+                  <a href={project.githubLink}>
+                    <Github />
+                    <span className="[@media(hover:hover)]:inline hidden">
+                      Github
+                    </span>
+                  </a>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>{project.githubLink}</TooltipContent>
+            </Tooltip>
+          </div>
+        </div>
+      </section>
+
+      <p className="text-muted-foreground">{project.description}</p>
+
+      <section className="space-y-6 border-l-3 border-muted">
+        <Item>
+          <ItemHeader>
+            <ItemTitle>Tech Stack</ItemTitle>
+          </ItemHeader>
+
+          <ItemContent>
+            <div className="flex gap-2 items-center flex-wrap">
+              {project.skills.map((skill, index) => (
+                <Badge variant="secondary" key={index}>
+                  {skill}
+                </Badge>
+              ))}
+            </div>
+          </ItemContent>
+        </Item>
+      </section>
+
+      <section className="grid gap-6">
+        <div className="grid md:grid-cols-2 2xl:grid-cols-3 gap-3">
+          {project.otherImages.map((image, index) => (
+            <div
+              key={index}
+              className="aspect-video overflow-hidden rounded-2xl"
+              onClick={() => open(image.url)}
+            >
+              <img src={image.url} alt="" />
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
